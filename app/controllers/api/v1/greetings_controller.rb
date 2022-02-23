0ed88d18f9caafcf5968f56b5552269a07b782d7
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-23 13:26:20 +0200

    Create an API endpoint

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
+    @greetings = Greeting.find(Greeting.pluck(:id).sample)
+    render json: {
+      greetings: @greetings
+    }.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      get 'greetings/index'
+    namespace :v1, defaults: { format: 'json' } do
+      get 'greetings', to: 'greetings#index'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
